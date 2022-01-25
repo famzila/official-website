00b3e55dfb6a1eba49358b8b894262087dc0db99
famzila <famzil.contact@gmail.com>
2022-01-25 20:28:59 +0100

    Add palette colors

--- src/App.js
+++ src/App.js
@@ -1,28 +1,38 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
 import { useTheme, ThemeProvider, createTheme } from "@mui/material/styles";
+import { styled } from "@mui/material/styles";
+import { amber, grey } from "@mui/material/colors";
+
 import CustomAppBar from "./components/AppBar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 
 const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
 
+const RootStyle = styled("main")(({ theme }) => ({
+  margin: "0",
+  padding: "0",
+  backgroundColor: theme.palette.background.default,
+  color: theme.palette.text.primary,
+}));
+
 function MyApp() {
-  const theme = useTheme();
-  const colorMode = React.useContext(ColorModeContext);
   return (
     <Box
-      component="main"
+      component="section"
       sx={{
-        margin: "80px",
+        padding: "80px",
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
         flexDirection: "column",
         minHeight: "560px",
+        backgroundColor: "background.default",
       }}
     >
       <Home />
+      <Footer />
     </Box>
   );
 }
@@ -43,6 +53,35 @@ export default function ToggleColorMode() {
       createTheme({
         palette: {
           mode,
+          primary: {
+            ...grey,
+          },
+          text: {
+            ...(mode === "light"
+              ? {
+                  primary: grey[300],
+                  secondary: grey[300],
+                  disabled: grey[500],
+                }
+              : {
+                  primary: grey[300],
+                  secondary: grey[300],
+                  disabled: grey[500],
+                }),
+          },
+          background: {
+            ...(mode === "light"
+              ? {
+                  paper: "#fff",
+                  default: "#fff",
+                  neutral: grey[200],
+                }
+              : {
+                  paper: "#292E3C",
+                  default: "#161C24",
+                  neutral: "black",
+                }),
+          },
         },
       }),
     [mode]
@@ -51,12 +90,13 @@ export default function ToggleColorMode() {
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
-        <CustomAppBar
-          dark={mode === "light" ? "dark" : "light"}
-          toggleColorMode={colorMode.toggleColorMode}
-        />
-        <MyApp />
-        <Footer />
+        <RootStyle>
+          <CustomAppBar
+            dark={mode === "light" ? "dark" : "light"}
+            toggleColorMode={colorMode.toggleColorMode}
+          />
+          <MyApp />
+        </RootStyle>
       </ThemeProvider>
     </ColorModeContext.Provider>
   );
