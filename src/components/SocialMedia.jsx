8c915150d06b73c074087c158d518e385d5561d8
famzila <famzil.contact@gmail.com>
2022-05-26 21:31:30 +0200

    Test 2 fix social media en prod

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -2,11 +2,20 @@ import React, { useEffect, useState } from "react";
 import { Avatar, Grid, Link } from "@mui/material";
 import { getSocialMediaIcons } from "../data/data";
 
+import { styled } from "@mui/material/styles";
 
 
 export default function SocialMedia(props) {
   const darkMode = props.dark;
   const [data, setData] = useState(getSocialMediaIcons(darkMode));
+
+  const StyledLink = styled(Link)({
+    height: 44,
+    overflow: "hidden",
+    WebkitLineClamp: 2,
+    display: "-webkit-box",
+    WebkitBoxOrient: "vertical",
+  });
 
   useEffect(() => {
     // Update icons based on dark mode
@@ -17,14 +26,20 @@ export default function SocialMedia(props) {
       {data.map((social, index) => {
         return (
           <Grid item key={index} sx={{ mt: 2}}>
-            <Link href={social.link} alt={social.name} target="_blank">
+            <StyledLink
+              href={social.link} alt={social.name} target="_blank"
+              variant="subtitle2"
+              color= "primary.contrastText"
+              underline="hover"
+            >
               <Avatar
-                href={social.link}
-                alt={social.name}
-                src={social.icon}
-                sx={{ width: 40, height: 40 }}
+                  target="_blank"
+                  href={social.link}
+                  alt={social.name}
+                  src={social.icon}
+                  sx={{ width: 40, height: 40 }}
               />
-            </Link>
+            </StyledLink>
           </Grid>
         );
       })}
